b5d7a07ea496f2dfb6997cffe1e03d12f5cfda63
Gleb Tsyganov <glebtsyganov1025@gmail.com>
2025-01-06 17:19:43 -0800

    portfolio first draft

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,91 +3,113 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className={styles.container}>
+      <header className={styles.header}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"/image.jpg"}
+          className={styles.image}
+          width={200}
+          height={200}
+          alt={"image"}
         />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
+        <p className={styles.headerText}>
+          <b>Gleb Tsyganov</b> <br /> <br />
+          product designer / entrepreneur
+        </p>
+      </header>
+      <div className={styles.projects}>
+        <div className={styles.project}>
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.trycrtd.com"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            <b className={styles.title}>crtd</b>
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+          <p className={styles.description}>
+            a social application to find out what your friends are <br />
+            reading / watching / listening to <br />
+            works by connecting your spotify, apple music, goodreads, letterboxd
+            account <br /> or by sharing links directly via SMS
+          </p>
         </div>
-      </main>
+        <div className={styles.project}>
+          <a
+            href="https://www.instagram.com/thezestclub/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b className={styles.title}>zest club</b>
+          </a>
+          <p className={styles.description}>
+            a private application based "speakeasy" for students <br />
+            to get in people had to text a story of a time when they impacted
+            someones life
+            <br />
+            first project to earn cash
+          </p>
+        </div>
+        <div className={styles.project}>
+          <a
+            href="https://x.com/GlebTsyganov/status/1232777636834877440"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b className={styles.title}>souply</b>
+          </a>
+          <p className={styles.description}>
+            an app that located free food around campus
+            <br />
+          </p>
+        </div>
+        <div className={styles.project}>
+          <a
+            href="https://www.thedotsgame.xyz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b className={styles.title}>dots</b>
+          </a>
+          <p className={styles.description}>
+            color guessing game like wordle <br /> <br />
+            <a href="/dots.png">image</a>
+          </p>
+        </div>
+        <div className={styles.project}>
+          <a
+            href="https://www.4-7-8.cc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b className={styles.title}>478</b>
+          </a>
+          <p className={styles.description}>
+            a breathing exercise to reduce anxiety <br /> <br />
+            <a href="/478demo.m4v">image</a>
+          </p>
+        </div>
+      </div>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="mailto:gleb@uwalumni.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          email
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://www.instagram.com/___gleb___25/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          instagram
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://x.com/GlebTsyganov"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          twitter
         </a>
       </footer>
     </div>
